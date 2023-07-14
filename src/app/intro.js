71baa8bfa7fd11dae914de70355a156b38e3f35c
DANLCARTON <ericthiberge@orange.fr>
2023-07-14 11:39:54 +0200

    meilleurs timings intro

--- src/app/intro.js
+++ src/app/intro.js
@@ -7,19 +7,17 @@ function getID(name) {
 
 
 async function intro() {
-    const delay = 2500
-    await wait(delay);
+    await wait(2000);
     clear();
     await updateText(["...", "..."], 80);
-    await wait(delay)
+    await wait(3480)
     clear();
     await updateText(["... *baillement* ..."], 80);
-    await wait(delay);
+    await wait(4440);
     clear();
     await updateText(["Hein ? Quoi ? Déjà ?", "Noooooon ! J'ai raté l'heure !"], 40);
-    await wait(delay);
+    await wait(4600);
     clear();
-
 }
 
 export {intro};
