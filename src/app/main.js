c2c6ecf243dd2645d95528e44da9dab6919b6325
DANLCARTON <ericthiberge@orange.fr>
2023-07-17 14:45:38 +0200

    Appel à mapStart

--- src/app/main.js
+++ src/app/main.js
@@ -1,23 +1,8 @@
 import { updateText, lireLigne, clear } from "./text.js";
 import {intro} from "./intro.js";
+import {mapStart} from "./map.js";
 
-
-/*
-hotkeys('ctrl+a,ctrl+b,r,f', function (event, handler){
-    switch (handler.key) {
-      case 'ctrl+a': alert('you pressed ctrl+a!');
-        break;
-      case 'ctrl+b': alert('you pressed ctrl+b!');
-        break;
-      case 'r': alert('you pressed r!');
-        break;
-      case 'f': alert('you pressed f!');
-        break;
-      default: alert(event);
-    }
-  });
-  */
-
-window.addEventListener("load", () => {
-    const id = intro();
+window.addEventListener("load", async () => {
+    const id = await intro();
+    await mapStart(id);
 })
